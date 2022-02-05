a44e3b008acec6e01d393c224b6bcab39ed63d9d
Alyssa Holland <alysam0071@gmail.com>
2022-02-05 00:52:37 -0500

    Wire up functionality on the 'Find Stations' Page

--- app/routes/find-stations/index.jsx
+++ app/routes/find-stations/index.jsx
@@ -1,8 +1,37 @@
-export default function FindStationsPage(){
+import { useNavigate } from "remix"
+import { useEffect, useState } from "react";
+import RadioGroupInput from "~/components/RadioGroupInput";
+
+import useDebounce from "~/hooks/useDebounce";
+
+const inputOptions = [
+    {
+        title: 'Use Current Location',
+        description: 'Requires permission to access GPS',
+        value: 'gps'
+    },
+    {
+        title: 'Manually Select Location',
+        description: 'Narrow down stations by state/city',
+        value: 'manual'
+    },
+]
+
+export default function FindStationsPage() {
+    const [selected, setSelected] = useState({ title: '', value: '' })
+    const debouncedValue = useDebounce(selected.value, 1000)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (debouncedValue === "gps") navigate("gps")
+        if (debouncedValue === "manual") navigate("manual")
+    }, [debouncedValue]);
+
     return (
         <div>
-            <h3>Find Stations Route</h3>
-            <p>Radio group will go here asking if they want a manual or current location search</p>
+            <RadioGroupInput options={inputOptions} onChange={(selection) => { setSelected(selection) }}
+                value={selected}
+            />
         </div>
     )
 }
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -4,12 +4,12 @@ import carImg from "../images/car.svg";
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-        <Link to="/find-stations">Find Stations</Link><br />
       <img
           style={{width: "50%"}}
           src={carImg}
           alt=""
       />
+        <Link to="/find-stations">Find Stations</Link><br />
     </div>
   );
 }
