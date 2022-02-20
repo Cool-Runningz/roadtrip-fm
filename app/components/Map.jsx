1d34d61576dd4e8ddccfe726febe6885b0225635
Alyssa Holland <alysam0071@gmail.com>
2022-02-19 19:02:52 -0500

    Add leaflet map to polish up coordinates display on GPS page

---
+++ app/components/Map.jsx
@@ -0,0 +1,50 @@
+import PropTypes from "prop-types"
+
+const propTypes = {
+    coordinates: PropTypes.shape({
+        latitude: PropTypes.number,
+        longitude: PropTypes.number
+    }).isRequired
+}
+
+const RADIUS_IN_METERS = 80467 //50 miles converted to meters for the Leaflet API
+let Leaflet = null
+
+//Dynamically import the leaflet module since it's dependent on the browsers 
+//window object and needs to be served client-side.
+import('react-leaflet')
+    .then((module) => {
+        Leaflet = module
+    }).catch(error => {
+        console.error("Error getting module")
+    });
+
+export default function Map(props) {
+    const { latitude, longitude } = props.coordinates
+
+    return (
+        <div>
+            {Leaflet ?
+                (
+                    <Leaflet.MapContainer center={[latitude, longitude]} zoom={7} style={{ width: "350px", height: "350px" }}>
+                        <Leaflet.TileLayer
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Leaflet.Circle center={[latitude, longitude]} pathOptions={{ fillColor: 'blue' }} radius={RADIUS_IN_METERS} />
+                        <Leaflet.Marker position={[latitude, longitude]}>
+                            <Leaflet.Popup>
+                                <p>
+                                    <span className="font-bold text-sm">Latitude: &nbsp;</span><code>{latitude}</code><br />
+                                    <span className="font-bold text-sm">Longitude: &nbsp;</span><code>{longitude}</code>
+                                </p>
+                            </Leaflet.Popup>
+                        </Leaflet.Marker>
+                    </Leaflet.MapContainer>
+                )
+                : <p className="text-lg font-bold"> Unable to Load Map </p>}
+        </div>
+    )
+}
+
+Map.propTypes = propTypes
--- app/routes/find-stations/gps.jsx
+++ app/routes/find-stations/gps.jsx
@@ -2,7 +2,16 @@ import { useEffect, useState } from "react";
 import { Link, useNavigate } from "remix";
 import { SearchIcon, ExclamationCircleIcon } from '@heroicons/react/solid'
 import Disclosure from "~/components/Disclosure";
+import Map from "~/components/Map"
 import radioTower from "~/images/RadioTower.svg";
+
+export function links() {
+    return [{
+        rel: "stylesheet", href: "https://unpkg.com/leaflet@1.7.1/dist/leaflet.css",
+        integrity: "sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==",
+        crossOrigin: ""
+    }]
+}
 
 const renderErrorMessage = (message) => {
     return (
@@ -16,7 +25,7 @@ const renderErrorMessage = (message) => {
                     <div className="mt-2 text-sm text-red-700">
                         <ul role="list" className="list-disc pl-5 space-y-1">
                             <li className="pb-2">Please ensure that proper permissions have been granted</li>
-                            <li className="pb-2">🔄 <Link className="underline" to="." reloadDocument>Retry Search</Link> </li>
+                            <li className="pb-2">🔄 <Link className="underline" to="." reloadDocument> Retry Search</Link> </li>
                             <li>Perform a <Link className="underline" to="/find-stations/manual"> manual search</Link></li>
                         </ul>
                     </div>
@@ -58,12 +67,17 @@ export default function GPSSearchPage() {
         navigator.geolocation.getCurrentPosition(successHandler, errorHandler, options);
     }, []);
 
+    /** Intentionally, handling the navigation with an imperative approach since there
+     *  may be a chance that I want to disable the button instead of hiding it by default.
+     *  Since this is not possible (at least in an accessible way) with native <a> tags 
+     *  having things setup this way makes things easier for potential refactor work.
+     */
     const handleClick = () => {
         navigate(`/stations-list?latitude=${coordinates.latitude}&longitude=${coordinates.longitude}`)
     }
 
     return (
-        <div className="max-w-md mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md mx-auto px-4 sm:px-6 lg:px-8 mt-8">
             <div className="max-w-sm mx-auto">
                 {loading && (
                     <div className="flex flex-col items-center">
@@ -72,17 +86,13 @@ export default function GPSSearchPage() {
                     </div>
                 )}
                 {!loading && coordinates.latitude && (
-                    <div>
-                        <div>
-                            <p>The coordinates are:</p>
-                            <code>Longitude: {coordinates.longitude}</code> <br />
-                            <code>Latitude: {coordinates.latitude}</code>
-                        </div>
+                    <div className="flexy-col-center">
+                        <Map coordinates={coordinates} />
                         <button
                             type="button"
                             disabled={loading || errorMsg}
                             onClick={handleClick}
-                            className="btn"
+                            className="btn mt-12"
                         >
                             Find Stations
                             <SearchIcon className="ml-3 -mr-1 icon-small" aria-hidden="true" />
@@ -94,7 +104,7 @@ export default function GPSSearchPage() {
                         {renderErrorMessage(`Unable to determine location: ${errorMsg}`)}
                     </div>
                 )}
-                <div className="w-80 md:w-96">
+                <div className="w-80 md:w-96 mt-20">
                     <Disclosure title="Disclaimer on GPS Search">
                         <p>GPS search leverages the <a href="https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API"> Geolocation API </a>. Permission will need to be granted in order to determine the location coordinates.
                             Your location is <strong className="underline decoration-wavy decoration-inherit text-purple-900">never stored</strong> and is purely used to determine radio stations near you.
