5cc500606228d7dc9dd839a4f0ed287ec3f5856c
Alyssa Holland <alysam0071@gmail.com>
2022-02-20 23:14:44 -0500

    Add CatchBoundary

--- app/root.jsx
+++ app/root.jsx
@@ -4,7 +4,8 @@ import {
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration
+  ScrollRestoration,
+  useCatch
 } from "remix";
 import styles from "./styles/app.css";
 import logo from "~/images/Logo.svg";
@@ -32,6 +33,36 @@ export function meta() {
     "twitter:description": description,
     "twitter:image": imageUrl
   };
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+  return (
+    <html className="h-full">
+      <head>
+        <title>404 Not Found</title>
+        <Links />
+      </head>
+      <body className="h-full">
+        <div className="min-h-full pt-16 pb-12 flex flex-col bg-white">
+          <main className="flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="py-16">
+              <div className="text-center">
+                <p className="text-sm font-semibold text-indigo-600 uppercase tracking-wide">{caught.status} error</p>
+                <h1 className="mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">{caught.statusText}</h1>
+                <p className="mt-2 text-base text-gray-500">Sorry, we couldn't find the page you're looking for.</p>
+                <div className="mt-6">
+                  <a href="/" className="text-base font-medium text-indigo-600 hover:text-indigo-500">
+                    Go back home
+                  </a>
+                </div>
+              </div>
+            </div>
+          </main>
+        </div>
+      </body>
+    </html>
+  );
 }
 
 export default function App() {
